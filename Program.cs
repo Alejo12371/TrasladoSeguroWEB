12bc5a5d4fa6b22c38980fcdf660d254b2fbe24d
Alejandro Parra <xjuanchito102371x@gmail.com>
2023-11-19 16:53:06 -0500

    Modelos y Configuracion de DbContext

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TrasladoSeguroWEB.Data;
+
 namespace TrasladoSeguroWEB
 {
     public class Program
@@ -8,6 +11,10 @@ namespace TrasladoSeguroWEB
 
             // Add services to the container.
             builder.Services.AddRazorPages();
+
+            builder.Services.AddDbContext<TrasladoSeguroContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("TrasladoSeguroDB"))
+            );
 
             var app = builder.Build();
 
