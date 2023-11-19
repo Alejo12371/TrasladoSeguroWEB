215e7953396718466ee9b0c6a7a5fa681ec05846
Alejandro Parra <xjuanchito102371x@gmail.com>
2023-11-19 17:05:17 -0500

    Index Clientes

---
+++ Pages/Clientes/Index.cshtml.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TrasladoSeguroWEB.Data;
+using TrasladoSeguroWEB.Models;
+
+namespace TrasladoSeguroWEB.Pages.Clientes
+{
+    public class IndexModel : PageModel
+    {
+        private readonly TrasladoSeguroContext _context;
+
+        public IndexModel(TrasladoSeguroContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Cliente> Clientes { get; set; } = default!;
+
+
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Clientes != null)
+            {
+                Clientes = await _context.Clientes.ToListAsync();
+            }
+
+        }
+        public void OnGet()
+        {
+        }
+    }
+}
