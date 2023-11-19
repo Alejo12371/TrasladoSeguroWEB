92c0e48dc0c171c7023c02d1b3ba4dbf8e6e4197
Alejandro Parra <xjuanchito102371x@gmail.com>
2023-11-19 17:43:17 -0500

    Index ServicioTransporte

---
+++ Pages/ServicioTransportes/Index.cshtml.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TrasladoSeguroWEB.Data;
+using TrasladoSeguroWEB.Models;
+
+namespace TrasladoSeguroWEB.Pages.ServicioTransportes
+{
+    public class IndexModel : PageModel
+    {
+        private readonly TrasladoSeguroContext _context;
+
+        public IndexModel(TrasladoSeguroContext context)
+        {
+            _context = context;
+        }
+
+        public IList<ServicioTransporte> ServicioTransportes { get; set; } = default!;
+
+
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Clientes != null)
+            {
+                ServicioTransportes = await _context.ServicioTransportes.ToListAsync();
+            }
+
+        }
+        
+    }
+}
