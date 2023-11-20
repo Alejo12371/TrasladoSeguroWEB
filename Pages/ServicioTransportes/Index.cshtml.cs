a7da5e3c1dadea30550668f9a4695a64e6074417
Alejandro Parra <xjuanchito102371x@gmail.com>
2023-11-19 19:05:59 -0500

    Autorizacion agregada a cada carpeta

--- Pages/ServicioTransportes/Index.cshtml.cs
+++ Pages/ServicioTransportes/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -6,6 +7,7 @@ using TrasladoSeguroWEB.Models;
 
 namespace TrasladoSeguroWEB.Pages.ServicioTransportes
 {
+    [Authorize]
     public class IndexModel : PageModel
     {
         private readonly TrasladoSeguroContext _context;
